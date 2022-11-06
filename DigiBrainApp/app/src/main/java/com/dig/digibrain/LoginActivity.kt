6de162d21cd6738bed611256a78d291cbeea116e
Coman Alex <comanalex33@yahoo.com>
2022-11-06 12:16:22 +0200

    Created registration page

--- DigiBrainApp/app/src/main/java/com/dig/digibrain/LoginActivity.kt
+++ DigiBrainApp/app/src/main/java/com/dig/digibrain/LoginActivity.kt
@@ -1,7 +1,9 @@
 package com.dig.digibrain
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
 import android.widget.Toast
 import com.dig.digibrain.databinding.ActivityLoginBinding
 
@@ -14,12 +16,29 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val animation = AnimationUtils.loadAnimation(this, R.anim.shake_animation)
+
         binding.loginButton.setOnClickListener {
-            Toast.makeText(this, "Check login credentials", Toast.LENGTH_SHORT).show()
+            binding.username.setBackgroundResource(R.color.gray)
+            binding.password.setBackgroundResource(R.color.gray)
+            when {
+                binding.username.text.toString() == "" -> {
+                    binding.username.setBackgroundResource(R.drawable.red_border)
+                    binding.usernameHolder.startAnimation(animation)
+                }
+                binding.password.text.toString() == "" -> {
+                    binding.password.setBackgroundResource(R.drawable.red_border)
+                    binding.passwordHolder.startAnimation(animation)
+                }
+                else -> {
+                    Toast.makeText(this, "Account logged in", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
 
         binding.registerButton.setOnClickListener {
-            Toast.makeText(this, "Go to registration page", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
         }
     }
 }
